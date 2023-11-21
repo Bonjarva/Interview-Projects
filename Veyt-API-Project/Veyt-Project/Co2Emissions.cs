d454d8ff1aaaf437810063f975f9c140a0f900d9
Bonjarva <jaydin.m@hotmail.com>
2023-11-21 20:26:47 +0100

    Created the Co2Emmsions class object, added csv

---
+++ Veyt-API-Project/Veyt-Project/Co2Emissions.cs
@@ -0,0 +1,28 @@
+namespace Veyt_Project;
+
+public class Co2Emissions
+{
+    //Example csv file data
+    //Country, String "New Zealand"
+    //Code, "NZL"
+    //CO2Emissions, 33276202
+    //YearlyChange, -0.14
+    //Percapita, 7.14
+    //Population, 4659265
+    //LifeExpectancy 81.862
+
+    public string? Country { get; set; }
+
+    public string? Code { get; set; }
+
+    public long CO2Emissions { get; set; }
+
+    public float YearlyChange { get; set; }
+
+    public float PerCapita { get; set; }
+
+    public long Population { get; set; }
+
+    public float LifeExpectancy { get; set; }
+
+}
