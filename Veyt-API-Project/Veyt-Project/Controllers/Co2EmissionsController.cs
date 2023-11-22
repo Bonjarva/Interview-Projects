c4cfd4eb17abfd610283b3795b928cc122383da8
Bonjarva <jaydin.m@hotmail.com>
2023-11-23 00:04:21 +0100

    replaced old functionality from base get

--- Veyt-API-Project/Veyt-Project/Controllers/Co2EmissionsController.cs
+++ Veyt-API-Project/Veyt-Project/Controllers/Co2EmissionsController.cs
@@ -21,25 +21,23 @@ public class Co2EmissionsController : ControllerBase
     //LifeExpectancy 81.862
 
     [HttpGet]
-    public IEnumerable<Co2Emissions> Get()
+    public IActionResult Get()
     {
 
-        //pull data out of the csv file ready for processing
-        var lines = System.IO.File.ReadAllLines(_csvPath);
-        var data = from line in lines.Skip(1)
-                   let columns = line.Split(',')
-                   select new Co2Emissions
-                   {
-                       Country = columns[0],
-                       Code = columns[1],
-                       CO2Emissions = long.Parse(columns[2]),
-                       YearlyChange = float.Parse(columns[3]),
-                       Percapita = float.Parse(columns[4]),
-                       Population = long.Parse(columns[5]),
-                       LifeExpectancy = float.Parse(columns[6])
-                   };
+        try
+        {
+            using var reader = new StreamReader(_csvPath);
+            using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture));
 
-        return data.ToList();
+            var records = csv.GetRecords<Co2Emissions>().ToList();
+            return Ok(records);
+
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"An error occurred: {ex.Message}");
+
+        }
     }
 
     [HttpGet("status")]
